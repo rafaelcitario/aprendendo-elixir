75109742d1fbb1616ff979bb8d9be107bf09e8fe
Rafael Citario <contato.rafaelgomes@outlook.com>
2024-05-23 16:06:56 -0300

    aprendendo elixir

---
+++ basic-types.exs
@@ -0,0 +1,11 @@
+IO.puts("
+	1 -> integer
+	0x1F -> integer
+	1.0 -> float
+	true -> boolean
+	false -> boolean
+	:atom -> atom / symbol
+	'elixir' -> string
+	[1, 2, 3, 4] -> list
+	{1, 2, 3, 4} -> tuple
+")
